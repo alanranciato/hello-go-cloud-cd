a7a34ff0fb7ba176c29a1498dcc3ca417479c324
Alan Ranciato <alanranciato@gmail.com>
2019-08-08 12:19:31 -0400

    new version

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "<div style: font-size 20px;font-weight:bold;>Hello <p/>Version 2</div>")
+        fmt.Fprintf(w, "<div style: font-size 20px;font-weight:bold;>Hello <p/>Version 3</div>")
     })
 
     http.ListenAndServe(":8080", nil)
