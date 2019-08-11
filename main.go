e874fd58ca8556aae7352062e35d4261f0c432ca
Alan Ranciato <alanranciato@gmail.com>
2019-08-11 14:48:28 -0400

    parameterize yaml

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const version string = "10.0"
+const version string = "11.0"
 
 func getFrontpage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Congratulations! Version %s of your application is running on GKE.", version)
